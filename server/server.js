03077579e181844a9bc24c3137f7a43b9eedee18
fenet roba <fenetroba700@gmail.com>
2025-03-05 22:53:47 +0300

    commited

--- server/server.js
+++ server/server.js
@@ -4,12 +4,13 @@ import Authrouter from "./Router/UserAuth.Router.js";
 import Ticketrouting from "./Router/ticketRoutes.js";
 import connectDB from "./Lib/DataBase.js";
 import cors from "cors";
+import path from 'path'
 import cookieParser from 'cookie-parser'
 env.config();
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
-
+const _dirname=path.resolve()
 const PORT = 5000;
 
 app.use(cors({
@@ -30,6 +31,15 @@ connectDB();
 
 app.use("/api/auth", Authrouter);
 app.use("/api/tickets", Ticketrouting);
+
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/client/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+	});
+}
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);  
 });
